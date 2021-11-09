e1f3bc41a65d37db653859f1ae0c9e4cf99f671d
Mark Andrews <mjandrews.org@gmail.com>
2021-11-09 13:03:04 +0000

    Fix week7.R (30815, 202122)

--- psyc30815/202122/week7.R
+++ psyc30815/202122/week7.R
@@ -1,154 +1,108 @@
+# sample some values from a Poisson distribution
+set.seed(1001)
+x <- rpois(100, lambda = 5)
+mean(x)
+var(x)
+var(x)/mean(x)
+
 library(tidyverse)
+biochem_df <- read_csv("http://data.ntupsychology.net/biochemist.csv")
 
-affairs_df <- read_csv(
-  "https://vincentarelbundock.github.io/Rdatasets/csv/AER/Affairs.csv") %>% 
-  select(-1) %>%
-  mutate(had_affair = affairs > 0)
+x <- biochem_df$publications
+mean(x)
+var(x)
+var(x)/mean(x)
 
-M1 <- glm(had_affair ~ age, 
-          data = affairs_df, family = binomial()
-)
-
-M2 <- glm(had_affair ~ age + yearsmarried,
-          data = affairs_df, family = binomial()
-)
-
-M0 <- glm(had_affair ~ 1, 
-          data = affairs_df, family = binomial()
-)
-
-M3 <- glm(had_affair ~ yearsmarried, 
-          data = affairs_df, family = binomial()
-)
+library(MASS)
 
 
-D0 <- deviance(M0)
-D1 <- deviance(M1)
-D2 <- deviance(M2)
-D3 <- deviance(M3)
+M <- glm.nb(publications ~ prestige, data = biochem_df)
 
-# Probability of getting a result greater than D0-D1
-# in a Chi square distribution with 1 df
-1 - pchisq(D0 - D1, df = 1)
-pchisq(D0 - D1, df = 1, lower.tail = F)
+summary(M)
+deviance(M) # does not work; is meaningless; ignore
+D <- (-2 * logLik(M))[[1]]
 
-pchisq(D0 - D2, df = 2, lower.tail = F)
-
-
-# Is there a sig predictive performance of yearsmarried
-# over and above the predictive performance of age
-pchisq(D1 - D2, df = 1, lower.tail = F)
-
-# Is there a sig predictive performance of age
-# over and above the predictive performance of yearsmarried
-
-pchisq(D3 - D2, df = 1, lower.tail = F)
-
-
-# yearsmarried * age 
-# yearsmarried + age + yearsmarried:age
-
-
-anova(M3, M2, test = 'Chisq')
-
-M4 <- glm(had_affair ~ 
-            gender + age + yearsmarried + 
-            children + religiousness + education + occupation + rating,
-          family = binomial(link = 'logit'),
-          data = affairs_df)
-
-M5 <- glm(had_affair ~ age + yearsmarried + religiousness + rating,
-          family = binomial(link = 'logit'),
-          data = affairs_df)
-
-anova(M4, M5, test='Chisq')
-
-
-
-# Count models ------------------------------------------------------------
-
-doctor_df <- read_csv("http://data.ntupsychology.net/DoctorAUS.csv")
-
-# model number of visits to doctor as Poisson dist
-# that is function of age
-M6 <- glm(doctorco ~ age, 
-          data = doctor_df, 
+# Poisson regression counterpart of M
+Mp <- glm(publications ~ prestige, 
+          data = biochem_df, 
           family = poisson(link = 'log'))
 
-summary(M6)
+# To test the null-hypothesis of equidispersion
+library(AER)
+dispersiontest(Mp, trafo = 1)
 
-estimates <- coef(M6)
-
-# What is log of the average number of visits 
-# for a person of median age, i.e. age = 0.32
-estimates[1] + estimates[2] * 0.32
-
-# What is the average number of visits 
-# for a person of median age, i.e. age = 0.32
-exp(estimates[1] + estimates[2] * 0.32)
+nbreg_deviance <- function(M) (-2 * logLik(M))[[1]]
 
 
-# some random samples from a Poisson with lambda = 0.25
-rpois(50, lambda = 0.25)
+summary(M)
 
+# extract the coefficients
+estimates <- coef(M)
 
+# predicted log of the mean of the number of pubs 
+# as a function of prestige from 1 to 5
+estimates[1] + estimates[2] * seq(5)
 
-# What is the average number of doctor visits
-# for a person of age = .75 (presumably, 75 years)
-exp(estimates[1] + estimates[2] * 0.75)
+# predicted mean of the number of pubs 
+# as a function of prestige from 1 to 5
+exp(estimates[1] + estimates[2] * seq(5))
 
-# some random samples from a Poisson with lambda = 0.25
-rpois(50, lambda = 0.48)
-
-
-# model number of visits to doctor as Poisson dist
-# that is function of sex (whose values are 0 and 1)
-M7 <- glm(doctorco ~ sex, 
-          data = doctor_df, 
-          family = poisson(link = 'log'))
-
-summary(M7)$coefficients
-
-estimates <- coef(M7)
-
-# what is log of average number of doctor visits 
-# for when sex = 0
-estimates[1] + estimates[2] * 0
-
-# what is log of average number of doctor visits 
-# for when sex = 1
-estimates[1] + estimates[2] * 1
-
-
-# difference in the log of the average between the two cases
-# i.e. difference between log of average when sex = 0 and when sex = 1
-# the following is identical to estimates[2]
-(estimates[1] + estimates[2] * 1) - (estimates[1] + estimates[2] * 0)
-
-# what is this?
-# this is the factor by which the average increases
-# as we go from sex = 0 to sex = 1
 exp(estimates[2])
 
-
-# what is average number of doctor visits 
-# for when sex = 0
-lambda_sex_0 <- exp(estimates[1] + estimates[2] * 0)
-
-# what is average number of doctor visits 
-# for when sex = 1
-lambda_sex_1 <- exp(estimates[1] + estimates[2] * 1)
-
-c(lambda_sex_1, lambda_sex_0, lambda_sex_1 / lambda_sex_0)
+M0 <- glm.nb(publications ~ 1, data = biochem_df)
+anova(M0, M)
 
 
-M8 <- glm(doctorco ~ sex + age + income + insurance,
-          data = doctor_df,
-          family = poisson(link = 'log'))
-M9 <- glm(doctorco ~ insurance,
-          data = doctor_df,
-          family = poisson(link = 'log'))
 
-c(deviance(M8), deviance(M9))
+# Zero inflated count models ----------------------------------------------
 
-anova(M9, M8, test = 'Chisq')
+smoking_df <- read_csv("http://data.ntupsychology.net/smoking.csv")
+
+barplot(table(smoking_df$cigs))
+
+library(pscl)
+
+Mzip <- zeroinfl(cigs ~ educ, data = smoking_df)
+Mzip_p <- glm(cigs ~ educ, data = smoking_df, 
+              family = poisson(link = 'log'))
+summary(Mzip_p)$coefficients
+
+
+coef(Mzip)
+summary(Mzip)
+
+estimates <- coef(Mzip)
+
+# predicted log odds of being a smoker if educ = 5, 10, 15, 20
+estimates[3] + estimates[4] * c(5, 10, 15, 20)
+
+# predicted probability of being a smoker if educ = 5, 10, 15, 20
+plogis(estimates[3] + estimates[4] * c(5, 10, 15, 20))
+
+# predicted log of the average no of cigs
+# smoked by a smoker with 5, 10, 15, or 20 years of educ
+estimates[1] + estimates[2] * c(5, 10, 15, 20)
+
+# predicted average no of cigs
+# smoked by a smoker with 5, 10, 15, or 20 years of educ
+exp(estimates[1] + estimates[2] * c(5, 10, 15, 20))
+
+
+smoking_df_2 <- tibble(educ = seq(20))
+library(modelr)
+
+# predicted probability of being a non-smoker
+# as a function of educ = 1 ... 20
+add_predictions(smoking_df_2, Mzip, type = 'zero')
+
+# predicted average number of cigs smoked by smokers
+# as a function of educ = 1 .... 20
+add_predictions(smoking_df_2, Mzip, type = 'count')
+
+# predicted average number of cigs smoked by smokers AND non-smokers
+# as a function of educ = 1 .... 20
+add_predictions(smoking_df_2, Mzip, type = 'response')
+
+# null hypothesis test comparing zero inflated to Poisson
+vuong(Mzip, Mzip_p)
+vuong(Mzip_p, Mzip) # same as previous, just numbers reversed
