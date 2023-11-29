941ae7966f5c370894abaf449a5f5c8f796b5a56
Mark Andrews <mjandrews.org@gmail.com>
2023-11-29 21:09:54 +0000

    Add content from week 8 to week 8+9 script

--- psyc30815/202324/workshop_week_8_9.R
+++ psyc30815/202324/workshop_week_8_9.R
@@ -1,8 +1,28 @@
-# Multilevel model --------------------------------------------------------
-
-#library(lme4)
 library(lmerTest)
 library(tidyverse)
+
+# visualize sleepstudy data
+ggplot(sleepstudy,
+       aes(x = Days, y = Reaction, colour = Subject)
+) + geom_point() + 
+  stat_smooth(method = 'lm', se = FALSE) + 
+  facet_wrap(~Subject)
+
+# simple linear regression; effect of sleep deprivation on RT
+M_8_1 <- lm(Reaction ~ Days, data = sleepstudy)
+
+# plot the simple linear model
+ggplot(sleepstudy,
+       aes(x = Days, y = Reaction)
+) + geom_point() + stat_smooth(method = 'lm', se = FALSE)
+
+summary(M_8_1)
+
+# prediction according to M_8_1
+sleepstudy_1 <- tibble(Days = c(0, 4, 9))
+
+library(modelr) # to load add_predictions
+add_predictions(sleepstudy_1, M_8_1)
 
 # Varying slope and varying intercept and possible correlations thereof
 M_9_1 <- lmer(Reaction ~ Days + (Days|Subject), data = sleepstudy)
